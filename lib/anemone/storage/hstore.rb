403743a28601346f1baf25858cfab816e627b55e
Jon Stokes <jon@paperhatpress.com>
2012-11-21 11:34:21 -0800

    Complete refactor of the way the hstore works

--- lib/anemone/storage/hstore.rb
+++ lib/anemone/storage/hstore.rb
@@ -5,39 +5,42 @@ module Anemone
   module Storage
     class Hstore
 
+      MARSHAL_FIELDS = %w(links visited fetched)
+
       def initialize(opts)
         @table = eval opts[:table_name]
         @key_prefix = opts[:key_prefix]
-        @table.delete_all
+        @table.where(:key_prefix => @key_prefix).delete_all
       end
 
       def [](key)
-        key = @key_prefix + key.to_s
-        row = @table.where("data ? '#{key}'")
-        return row.empty? ? nil : row.first.data[key]
+        rget(key.to_s)
       end
 
       def []=(key,value)
-        key = @key_prefix + key.to_s
-        @table.create(:data => {key => value})
+        hash = value.to_hash
+        MARSHAL_FIELDS.each do |field|
+          hash[field] = Marshal.dump(hash[field])
+        end
+        if row = @table.find_by_key(key.to_s)
+          row.update_attribute(:data, hash)
+        else
+          @table.create(:key => key.to_s, :key_prefix => @key_prefix, :data => hash)
+        end
       end
 
       def delete(key)
-        key = @key_prefix + key.to_s
-        row = @table.where("data ? '#{key}'")
-        page = nil
-        unless row.empty?
-          page = row.first.data[key]
-          row.first.data[key].destroy_key(:data, key)
-        end
-        return page
+        page = self[key]
+        @table.find_by_key(key.to_s).try(:destroy)
+        page
       end
 
       def each
-        @table.all.each do |row|
-          hash = row.data
-          hash.each do |key, value|
-            yield key, value
+        @table.where(:key_prefix => @key_prefix).find_each do |row|
+          hash = row.try(:data)
+          if !!hash
+            page = load_value(hash)
+            yield row.key, page
           end
         end
       end
@@ -63,10 +66,29 @@ module Anemone
       end
 
       def size
-        @table.all.count
+        @table.where(:key_prefix => @key_prefix).count
       end
 
       def close; end
+
+      private
+
+      def load_value(hash)
+        MARSHAL_FIELDS.each do |field|
+          unless hash[field].nil? || hash[field] == ''
+            hash[field] = Marshal.load(hash[field])
+          end
+        end
+        Page.from_hash(hash)
+      end
+
+      def rget(rkey)
+        page = @table.where(:key_prefix => @key_prefix, :key => rkey)
+        hash = page.try(:data)
+        if !!hash
+          load_value(hash)
+        end
+      end
 
     end
   end
