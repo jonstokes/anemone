5e9c6e737e001c13ac7f3d85d43db594a15499b2
Jon Stokes <jon@paperhatpress.com>
2013-02-07 19:54:22 -0800

    Fixed retrieve_queue_url

--- lib/anemone/sqs_queue.rb
+++ lib/anemone/sqs_queue.rb
@@ -160,7 +160,7 @@ class SqsQueue
   end
 
   def retrieve_queue_url
-    response = @sqs.list_queues(:QueueNamePrefix => queue_name)
+    response = @sqs.list_queues("QueueNamePrefix" => queue_name)
     list = response.body ? response.body["QueueUrls"] : []
     list.detect { |url| url.index(queue_name) && (url[(url.index(queue_name)-1)..-1] == "/#{queue_name}") }
   end
